806d1e9a7f860e7477b872d50c22c08cc775b0bd
williamberry-mrb <williamberry@iusd.org>
2026-02-10 11:53:12 -0800

    Update BinaryTree.java

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,5 +1,1 @@
-public interface BinaryTree<E> extends Tree<E> {
-    Position<E> left(Position<E> p) throws IllegalArgumentException;
-    Position<E> right(Position<E> p) throws IllegalArgumentException;
-    Position<E> sibling(Position<E> p) throws IllegalArgumentException;
-}
+
